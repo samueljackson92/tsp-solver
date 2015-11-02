b1f57b3ee9f5c8068a0705b3cae11edd47fa0fba
Samuel Jackson <samueljackson@outlook.com>
2015-11-02 17:20:56 +0000

    Refs #4 Add docstrings

--- tspsolver/ga/selection.py
+++ tspsolver/ga/selection.py
@@ -6,6 +6,12 @@ class AbstractSelection(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, distance_matrix, subset_size):
+        """Create a new selection technique.
+
+        :param distance_matrix: the distance matrix of points in the dataset
+        :param subset_size: the size of the subset of the population to use.
+        """
+
         self._distance_matrix = distance_matrix
         self._population_size = distance_matrix.shape[0]
         self._subset_size = subset_size
@@ -35,12 +41,27 @@ class AbstractSelection(object):
         return np.array([self._fitness_for_chromosome(x) for x in population])
 
     def _fitness_for_chromosome(self, chromosome):
+        """Compute the fitness for a single chromosome
+
+        :param chromosome: 1D array encoding to calcuate fitness for
+        :return: 1D array of fitness estimates
+        :rtype: ndarray
+        """
+
         point_indices = zip(chromosome, np.roll(chromosome, 1))
         distances = np.array([self._distance_matrix[i, j]
                              for i, j in point_indices])
         return distances.sum()
 
     def _normalise_fitness(self, fitness):
+        """Normalise the output of the fitness function.
+
+        This allows them to be interepted as probabilities.
+
+        :param fitness: 1D array of fitness estimates
+        :return: 1D array of normalised fitness estimates
+        :rtype: ndarray
+        """
         total_fitness = fitness.sum()
         return np.array([f / total_fitness for f in fitness])
 
@@ -48,6 +69,17 @@ class AbstractSelection(object):
 class RouletteWheelSelection(AbstractSelection):
 
     def _choose_subset(self, population, fit_prob):
+        """Choose a random subset of te population weighted by their fitness
+        probability.
+
+        Chromosomes with a higher fitness (greater probability) are more likely
+        to be selected than other individuals.
+
+        :param population: 2D array representing the population of solutions.
+        :param fit_prob: 1D array representing the normalised fitness.
+        :return: a subset of the population
+        :rtype: ndarray
+        """
         pop_size = population.shape[0]
         indicies = np.arange(pop_size)
         idx = np.random.choice(indicies, size=self._subset_size, p=fit_prob)
