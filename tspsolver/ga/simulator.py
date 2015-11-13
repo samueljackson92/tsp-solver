09a3c2a15751d8213b7a5549e8bd63792b727579
Samuel Jackson <samueljackson@outlook.com>
2015-11-13 20:11:55 +0000

    Refactor simulator and add elitism

--- tspsolver/ga/simulator.py
+++ tspsolver/ga/simulator.py
@@ -3,11 +3,12 @@ import numpy as np
 
 class Simulator:
 
-    def __init__(self, generator, selector, crossover, mutator):
+    def __init__(self, generator, selector, crossover, mutator, num_elites=2):
         self._generator = generator
         self._selector = selector
         self._crossover = crossover
         self._mutator = mutator
+        self._num_elites = num_elites
 
         self._average_fitness = []
         self._min_fitness = []
@@ -17,19 +18,33 @@ class Simulator:
         population = self.initilize_population()
 
         for i in xrange(num_epochs):
-            subset = self.perform_selection(population)
-            new_population = self.perform_crossover(subset)
-            new_population = self.perform_mutation(new_population)
-            population = new_population
+            population = self._apply_genetic_operations(population)
+            self._cache_epoch_performance(population)
 
-            if i % 1 == 0:
-                fitness = self._selector.fitness(population)
-                self._average_fitness.append(fitness.mean())
-                self._min_fitness.append(fitness.min())
-                self._max_fitness.append(fitness.max())
-                best = population[np.argmin(fitness)]
+        return self._find_best_solution(population)
 
-        return best
+    def _cache_epoch_performance(self, population):
+        fitness = self._selector.get_fitness()
+        self._average_fitness.append(fitness.mean())
+        self._min_fitness.append(fitness.min())
+        self._max_fitness.append(fitness.max())
+
+    def _apply_genetic_operations(self, population):
+        subset = self.perform_selection(population)
+        new_population = self.perform_crossover(subset)
+        new_population = self.perform_mutation(new_population)
+        new_population = self._apply_elitism(population, new_population)
+        return new_population
+
+    def _apply_elitism(self, population, new_population):
+        fitness = self._selector.get_fitness()
+        sorted_population = population[np.argsort(fitness)]
+        new_population[:self._num_elites] = sorted_population[:self._num_elites]
+        return new_population
+
+    def _find_best_solution(self, population):
+        fitness = self._selector.get_fitness()
+        return population[np.argmin(fitness)]
 
     def get_averge_fitness(self):
         return self._average_fitness
