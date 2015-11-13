cf708d13aa8dc74e0d9049d62b0adb1fd38f88be
Samuel Jackson <samueljackson@outlook.com>
2015-11-13 20:13:38 +0000

    Update simualtor test.

--- tspsolver/test/simulator_test.py
+++ tspsolver/test/simulator_test.py
@@ -5,24 +5,24 @@ import numpy as np
 from ..generator.tsp_generator import TSPGenerator
 from ..ga.simulator import Simulator
 from ..ga.population_generation import SimplePopulationGenerator
-from ..ga.selection import RouletteWheelSelection
-from ..ga.crossover import OnePointPMX
-from ..ga.mutation import SwapCityMutation
+from ..ga.selection import RouletteWheelSelection, TournamentSelection
+from ..ga.crossover import TwoPointPMX
+from ..ga.mutation import SwapCityMutation, SwapAdjacentCityMutation
 
 
 class SimulatorTest(unittest.TestCase):
 
     def setUp(self):
-        self._num_points = 10
+        self._num_points = 30
         gen = TSPGenerator(self._num_points)
         self._data, self._distances = gen.generate()
 
     def test_evolve(self):
         sim = Simulator(
             generator=SimplePopulationGenerator(100, self._distances),
-            selector=RouletteWheelSelection(self._distances, 30),
-            crossover=OnePointPMX(pcross=0.6),
-            mutator=SwapCityMutation(p=0.05)
+            selector=TournamentSelection(self._distances, 20, tournament_size=5),
+            crossover=TwoPointPMX(pcross=0.6),
+            mutator=SwapCityMutation(p=0.2)
         )
 
         solution = sim.evolve(num_epochs=1000)
