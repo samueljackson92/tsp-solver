fe541610d9d7a3a19429447740f47c744264e1d3
Samuel Jackson <samueljackson@outlook.com>
2015-12-08 08:21:32 +0000

    Fix setup.

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
-import tspsolver
-
 config = {
     'description': 'TSP Solver with GAs',
     'author': 'Samuel Jackson',
     'url': 'http://github.com/samueljackson92/tsp-solver',
     'download_url': 'http://github.com/samueljackson92/tsp-solver',
     'author_email': 'samueljackson@outlook.com',
-    'version': tspsolver.__version__,
+    'version': '0.1.0',
     'install_requires': [
         'numpy',
         'scipy',
